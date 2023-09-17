b2cc16ada3380af7a4cf3ec46141065ef43450d2
Corentin Laudrin <corentin.laudrin@gmail.com>
2023-09-17 17:07:04 -0300

    premier push 17/09/23

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+
+import France from "./flags/france.png";
 
 const Tab1: React.FC = () => {
   return (
@@ -17,6 +24,14 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <div className=" mt-8">
+          <div className="flex justify-center gap-6">
+            <img className="h-8" src="/flags/france.png" alt="france" />
+            <img className="h-8" src="/flags/uk.png" alt="usa" />
+            <img className="h-8" src="/flags/brasil.png" alt="brasil" />
+            <img className="h-8" src="/flags/russia.png" alt="russia" />
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );

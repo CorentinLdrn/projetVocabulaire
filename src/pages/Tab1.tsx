971ece40e4a7ef0daafdf8d82cd06ed950d084c2
Corentin Laudrin <corentin.laudrin@gmail.com>
2023-12-05 17:01:51 -0300

    embedded edt

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -23,13 +23,22 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
         <div className=" mt-8">
           <div className="flex justify-center gap-6">
             <img className="h-8" src="/flags/france.png" alt="france" />
             <img className="h-8" src="/flags/uk.png" alt="usa" />
             <img className="h-8" src="/flags/brasil.png" alt="brasil" />
             <img className="h-8" src="/flags/russia.png" alt="russia" />
+          </div>
+          <div>
+            <iframe
+              className="border-0 frame"
+              src="https://calendar.google.com/calendar/embed?src=corentin.laudrin%40gmail.com&ctz=America%2FSao_Paulo"
+              width="800"
+              height="600"
+              frameBorder="0"
+              scrolling="no"
+            ></iframe>
           </div>
         </div>
       </IonContent>
